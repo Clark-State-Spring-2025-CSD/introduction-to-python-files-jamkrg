abb419e2582393d7adbf2ebba1dfb1bcb0f91af2
James Karg <kargj4496@clarkstate.edu>
2025-01-29 13:12:25 -0500

    Update AdditionDemo.py

---
+++ AdditionDemo.py
@@ -0,0 +1,9 @@
+number1 = 0
+number2 = 0
+
+print("Please enter two numbers")
+number1 = int(input("Number 1: "))
+number2 = int(input("Number 2: "))
+
+sum = number1 + number2
+print(f"The sum of {number1} and {number2} is {sum}.")
